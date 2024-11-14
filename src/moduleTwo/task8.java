7c3bb98197d21827d1dc29e9ba4c4a5a3fae1277
Kirill <62447478+Kir14@users.noreply.github.com>
2024-11-14 15:53:55 +0300

    2 ways

--- src/moduleTwo/task8.java
+++ src/moduleTwo/task8.java
@@ -15,6 +15,30 @@ package moduleTwo;
  */
 public class task8 {
     public static void main(String[] args) {
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 3; j >= 0; j--) {
+                if (i >= j) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (i <= j) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
+
+
         for (int i = 1; i <= 4; i++) {
             StringBuilder str = new StringBuilder();
             for (int j = 0; j < i; j++) {
