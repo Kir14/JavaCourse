fcd896c7e89ec401a21cae081bccf049629d953b
Kirill <62447478+Kir14@users.noreply.github.com>
2024-11-14 11:06:09 +0300

    Exersices 4,5

--- HomeWork2/src/Main.java
+++ HomeWork2/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Exersice1 = " + exersice1() + " км");
@@ -6,6 +8,12 @@ public class Main {
         exersice2();
 
         System.out.println("Exersice3 = " + exersice3());
+
+        System.out.println("Exersice4");
+        System.out.println("a*b= " + exersice4());
+
+        System.out.println("Exersice5");
+        exersice5();
     }
 
     public static double exersice1() {
@@ -24,7 +32,7 @@ public class Main {
         int time = 3;
         int start = 1;
         int countOfDivisions = 2;
-        for (int i = 3; i < 25; i+=time) {
+        for (int i = 3; i < 25; i += time) {
             start *= countOfDivisions;
             System.out.println("Time " + i + " count of ameb = " + start);
         }
@@ -33,10 +41,32 @@ public class Main {
     public static int exersice3() {
         int start = 1;
         int sum = 0;
-        while(start < 257){
+        while (start < 257) {
             sum += start;
             start *= 2;
         }
         return sum;
+    }
+
+    public static int exersice4() {
+        int a, b;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("a = ");
+        a = scanner.nextInt();
+        System.out.print("b = ");
+        b = scanner.nextInt();
+        int sum = 0;
+        while (b > 0) {
+            sum += a;
+            b--;
+        }
+        return sum;
+    }
+
+    public static void exersice5() {
+        System.out.println("| inch |  cm  |");
+        for (int i = 1; i < 21; i++) {
+            System.out.println("|" + String.format("%-6s", i) + "|" + String.format("%6s", i * 2.54) + "|");
+        }
     }
 }
