aa5735cabbd8efc89f230eb600ce10108cc6138a
Kirill <62447478+Kir14@users.noreply.github.com>
2024-11-15 15:44:40 +0300

    2 ways

--- src/moduleOne/task7.java
+++ src/moduleOne/task7.java
@@ -7,19 +7,31 @@ import java.util.Scanner;
  зависимости от количества программистов необходимо вывести правильно
  окончание. Например:
  • 2 программиста
- • 1 программиста
+ • 1 программист
  • 10 программистов
  • и т.д
  */
 public class task7 {
     public static void main(String[] args) {
-        int n;
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        if ((n % 10 < 5) && (n % 10 != 0) && !((n > 10) && (n < 15))) {
-            System.out.println(n + " программиста");
-        } else {
-            System.out.println(n + " программистов");
+        //Scanner scanner = new Scanner(System.in);
+        for (int n = 1; n <= 200; n++) {
+            if ((n % 10 == 1) && (n % 100 != 11)) {
+                System.out.println(n + " программист");
+            }else if((n % 10 < 5) && (n % 10 != 0) && !((n % 100 > 10) && (n % 100 < 15))) {
+                System.out.println(n + " программиста");
+            } else {
+                System.out.println(n + " программистов");
+            }
+        }
+
+        for (int number = 0; number <= 200; number++) {
+            System.out.print(number + " программист");
+            if ((number % 10 > 4) || (number % 10 == 0) || ((number % 100 > 10) && (number % 100 < 20))) {
+                System.out.print("ов");
+            } else if ((number % 10 > 1) && (number % 10 < 5)) {
+                System.out.print("а");
+            }
+            System.out.println();
         }
     }
 }
