b531fe9a69d2bd18c44632f8b880fe4e3a8e5b93
Kirill <62447478+Kir14@users.noreply.github.com>
2024-11-15 11:17:46 +0300

    task1

---
+++ src/moduleThree/HomeWork/Cat/src/Loader.java
@@ -0,0 +1,31 @@
+package moduleThree.HomeWork.Cat.src;
+
+import java.util.Objects;
+
+public class Loader
+{
+    public static void main(String[] args)
+    {
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        Cat cat3 = new Cat();
+        Cat cat4 = new Cat();
+
+        System.out.println(cat1.getWeight());
+        cat1.feed(10.0);
+        System.out.println(cat1.getWeight());
+
+        System.out.println(cat2.getWeight());
+        cat2.feed(12.0);
+        System.out.println(cat2.getWeight());
+
+        cat3.feed(100000.0);
+        System.out.println(cat3.getStatus());
+
+        while (!Objects.equals(cat4.getStatus(), "Dead")) {
+            cat4.meow();
+        }
+        System.out.println(cat4.getStatus());
+
+    }
+}
