33e2617734e1dc4563d97a6c4be1d915d85d2915
Kirill <62447478+Kir14@users.noreply.github.com>
2024-11-16 15:20:49 +0300

    comp upd

--- src/moduleThree/HomeWork/comp/src/Main.java
+++ src/moduleThree/HomeWork/comp/src/Main.java
@@ -2,9 +2,10 @@ package moduleThree.HomeWork.comp.src;
 
 public class Main {
     public static void main(String[] args) {
-        Computer computer = new Computer("intel", 12, "Huge", 4);
+        Computer computer = new Computer("intel", 12, "Huge", 2);
         System.out.println(computer);
-        for (int i = 0; i < computer.getTimeToDie(); i++) {
+        int n = computer.getTimeToDie() - 1;
+        for (int i = 0; (i < n) && (computer.getTimeToDie() != 0); i++) {
             computer.turnOn();
             computer.turnOff();
         }
