9f4ded5fe7b8e17134d475049b321a6cd8a4268d
Kirill <62447478+Kir14@users.noreply.github.com>
2024-11-18 10:15:16 +0300

    Character

--- src/moduleFour/IntegerExperiments/src/Main.java
+++ src/moduleFour/IntegerExperiments/src/Main.java
@@ -6,8 +6,12 @@ public class Main {
         container.count += 7843;
 
         System.out.println(sumDigits(12345));
+        System.out.println(sumDigitsWayTwo(12345));
         System.out.println(sumDigits(10));
+        System.out.println(sumDigitsWayTwo(10));
         System.out.println(sumDigits(5059191));
+        System.out.println(sumDigitsWayTwo(5059191));
+
 
     }
 
@@ -16,6 +20,15 @@ public class Main {
         int sum = 0;
         for (int i = 0; i < strNumber.length(); i++) {
             sum += Integer.parseInt(String.valueOf(strNumber.charAt(i)));
+        }
+        return sum;
+    }
+
+    public static Integer sumDigitsWayTwo(Integer number) {
+        String strNumber = number.toString();
+        int sum = 0;
+        for (int i = 0; i < strNumber.length(); i++) {
+            sum += Character.getNumericValue(strNumber.charAt(i));
         }
         return sum;
     }
