ebc001d5820a693575b64d770adf31af07c9b5d5
Kirill <62447478+Kir14@users.noreply.github.com>
2024-12-18 11:25:37 +0300

    exception

---
+++ src/main/java/modules/seven/exception/consolecustomer/src/CustomerStorage.java
@@ -0,0 +1,41 @@
+package modules.seven.exception.consolecustomer.src;
+
+import java.util.HashMap;
+
+public class CustomerStorage {
+    private HashMap<String, Customer> storage;
+
+    public CustomerStorage() {
+        storage = new HashMap<>();
+    }
+
+    public void addCustomer(String data) throws IllegalArgumentException {
+        String[] components = data.split("\\s+");
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Неверное количество аргументов");
+        }
+        if (!components[2].matches("^[A-Za-z](\\.?\\w)*@[A-Za-z]+\\.[A-Za-z]+$")) {
+            throw new IllegalArgumentException("Неверный формат ввода почты");
+        }
+        if (!components[3].matches("^\\+\\d+")) {
+            throw new IllegalArgumentException("Неверный формат ввода номера телефона");
+        }
+        String name = components[0] + " " + components[1];
+        storage.put(name, new Customer(name, components[3], components[2]));
+    }
+
+    public void listCustomers() {
+        storage.values().forEach(System.out::println);
+    }
+
+    public void removeCustomer(String name) throws IllegalArgumentException {
+        if (!storage.containsKey(name)) {
+            throw new IllegalArgumentException("Такого покупателя нет");
+        }
+        storage.remove(name);
+    }
+
+    public int getCount() {
+        return storage.size();
+    }
+}
